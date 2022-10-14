759db823f1efbddf685eeed754461e8cdd2915e2
Sanjay1736 <92837525+Sanjay1736@users.noreply.github.com>
2022-10-14 14:18:19 +0530

    learning

---
+++ int.js
@@ -0,0 +1,46 @@
+// function closure(){
+//     var a=8;
+//     function inner(){
+//         console.log(a);
+//     }
+// }
+// function abc() {
+
+//     a=10;
+    
+//     }
+    
+//    // abc()
+    
+//     console.log(a)
+
+// function abc() {
+
+//     if(true) {
+    
+//     var a=10;
+    
+//     }
+    
+//     console.log(a)
+    
+//     }
+    
+//     abc()
+
+
+    // function abc(){
+    //     console.log("hello");
+    // }
+
+    // function call(abc){
+    //     abc();
+    // }
+    
+    function abc(){
+        const name=document.getElementById("name").value;
+        const email=document.getElementById("email").value;
+        const phone=document.getElementById("phone").value;
+        alert(name+email+phone);
+
+    }
