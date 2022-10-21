66dbaf0cd0e315ffbcf3097e9fc42e79c6c3ceca
Sanjay1736 <92837525+Sanjay1736@users.noreply.github.com>
2022-10-21 13:51:47 +0530

    changes

--- intr.js
+++ intr.js
@@ -1,17 +1,17 @@
-// //const value=false;
-// // let val= new Promise((resolve,reject)=>{
-// //     if(value){
-// //         resolve(console.log("yes"));
-// //     }
-// //     else{
-// //         reject(console.log("No"));
-// //     }
-// // })
+const value=false;
+let val= new Promise((resolve,reject)=>{
+    if(value){
+        resolve(console.log("yes"));
+    }
+    else{
+        reject(console.log("No"));
+    }
+})
 
 function* valuee(){
-    // for(let i=1;i<=10;i++){
-    //     yield i;
-    // }
+    for(let i=1;i<=10;i++){
+        yield i;
+    }
     yield 1;
     yield 2;
     return 3;
@@ -19,60 +19,60 @@ function* valuee(){
 
 const gen=valuee();
 console.log(valuee.next().value);
-//console.log(valuee.next);
+console.log(valuee.next);
 
-// const arr=[1,2,3,4];
-// const arr2=[...arr];
+const arr=[1,2,3,4];
+const arr2=[...arr];
 
-// function value(...arr){
+function value(...arr){
 
-// }
-// function greet(person) {
+}
+function greet(person) {
 
-//     if(person == {name : 'Newton' }){
+    if(person == {name : 'Newton' }){
     
-//     console.log('hey newton');
+    console.log('hey newton');
     
-//     }else{
+    }else{
     
-//     console.log('hey there');
+    console.log('hey there');
     
-//     }
+    }
     
-//     }
+    }
     
-//     greet({name : 'Newton' });
+    greet({name : 'Newton' });
 
 
-//     const a = {"b": "hey"};
+    const a = {"b": "hey"};
 
-//     const d = {"b": "hey"};
+    const d = {"b": "hey"};
     
-//     console.log(a == d);
+    console.log(a == d);
  
-//     const val1={
-//         id:21,
-//         Name:"Sanjay",
-//     }
-//     const val2={
-//         id:21,
-//         Name:"Sanjay",
-//     }
+    const val1={
+        id:21,
+        Name:"Sanjay",
+    }
+    const val2={
+        id:21,
+        Name:"Sanjay",
+    }
 
     
     
-//     function test(){
-//      let x = y = 0;
-//      x++; y++; 
-//      return x;
-//    }
-//    console.log(test(), typeof x, typeof y);
+    function test(){
+     let x = y = 0;
+     x++; y++; 
+     return x;
+   }
+   console.log(test(), typeof x, typeof y);
 
-//    let y = 1;
-//    if(function g(){}) {
-//        y += typeof g;
-//      }
-//     console.log(y);
+   let y = 1;
+   if(function g(){}) {
+       y += typeof g;
+     }
+    console.log(y);
 
 
    
--- jsleraning/pracrtise.js
+++ jsleraning/pracrtise.js
@@ -39,3 +39,35 @@ sumOfThreeElements(1,2,3)
 sumOfThreeElements(1,5,7,8)
 .then(res=>console.log(res))
 .catch(err=>console.log(err));
+
+const obj1={
+    id:121,
+    Name:"sanjay",
+    Age:21,
+}
+
+const obj2={
+    id:121,
+    Name:"sanjay",
+    Age:21,
+}
+
+if(obj1===obj2){
+    console.log("true");
+}
+else{
+    console.log("false")
+}
+
+
+var eq = JSON.stringify(obj1) === JSON.stringify(obj2);
+console.log(eq);
+
+function sayHi() {
+  console.log(name);
+  console.log(age);
+  var name = 'Ayush';
+  let age = 21;
+}
+
+sayHi();
